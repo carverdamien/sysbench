808deea14d091543db4cc0e298dbfdbd35b8056d
Damien Carver <carverdamien@gmail.com>
2016-11-03 18:39:28 +0100

    use docker create volume

--- prepare.sh
+++ prepare.sh
@@ -2,11 +2,10 @@
 set -e -x
 DBNAME=$1
 DBSIZE=$2
-IMAGE="${DBNAME}-${DBSIZE}"
-docker run -d --name=mysql -e MYSQL_ALLOW_EMPTY_PASSWORD=yes --net=host mysql:latest
+VOLUME="${DBNAME}-${DBSIZE}"
+VPATH='/var/lib/mysql'
+docker volume create --name "$VOLUME"
+docker run -d --name=mysql -v "$VOLUME:$VPATH" -e MYSQL_ALLOW_EMPTY_PASSWORD=yes --net=host mysql:latest
 until docker exec mysql mysql --host 127.0.0.1 -u root -e "CREATE DATABASE ${DBNAME}"; do sleep 10; done
 docker run --rm --entrypoint=./sysbench/sysbench --net=host sysbench:latest --test=./sysbench/tests/db/oltp.lua --oltp-table-size=${DBSIZE} --mysql-db=${DBNAME} --mysql-host=127.0.0.1 --mysql-user=root --mysql-password= prepare
-docker stop mysql
-docker tag $(docker commit mysql) ${IMAGE}
 docker rm -fv mysql
-
